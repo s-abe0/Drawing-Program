43a6964b22159d316f50ecc3c981d6199315db94
redbullx33 <sabe0@frostburg.edu>
2017-02-18 15:26:22 -0500

    first commit

---
+++ Draw.java
@@ -0,0 +1,111 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.paint.Color;
+
+public class Draw extends Application 
+{
+    Color currentColor = Color.BLACK;
+    Pane pane = new Pane();
+    Circle circle = new Circle(50, 50, 5, Color.BLACK);
+    
+    @Override
+    public void start(Stage primaryStage) 
+	{
+        Menu colorsMenu = new Menu("Colors");
+        MenuItem black = new MenuItem("Black");
+        MenuItem red = new MenuItem("Red");
+        MenuItem blue = new MenuItem("Blue");
+        MenuBar menu = new MenuBar();
+        colorsMenu.getItems().addAll(black, red, blue);
+        menu.getMenus().add(colorsMenu);
+        
+        pane.getChildren().addAll(circle, menu);
+
+        black.setOnAction(e -> 
+		{
+           currentColor = Color.BLACK;
+           circle.setFill(currentColor);
+        });
+        
+        blue.setOnAction(e -> 
+		{
+           currentColor = Color.BLUE;
+           circle.setFill(currentColor);
+        });
+        
+        red.setOnAction(e -> 
+		{
+           currentColor = Color.RED;
+           circle.setFill(currentColor);
+        });
+        
+        circle.setOnKeyPressed(e -> 
+		{
+            switch (e.getCode())
+			{
+                case DOWN:
+                    circle.setCenterY(circle.getCenterY() + 5);
+                    if (e.isShiftDown()) 
+					{
+                        createLine().setEndY(circle.getCenterY());
+                        createLine().setEndY(circle.getCenterY() - 5);
+                    }
+                    break;
+                case UP:
+                    circle.setCenterY(circle.getCenterY() - 5);
+                    if (e.isShiftDown()) 
+					{
+                        createLine().setEndY(circle.getCenterY());
+                        createLine().setEndY(circle.getCenterY() + 5);
+                    }
+                    break;
+                case LEFT:
+                    circle.setCenterX(circle.getCenterX() - 5);
+                    if (e.isShiftDown()) 
+					{
+                        createLine().setEndX(circle.getCenterX());
+                        createLine().setEndX(circle.getCenterX() + 5);
+                    }
+                    break;
+                case RIGHT:
+                    circle.setCenterX(circle.getCenterX() + 5);
+                    if (e.isShiftDown()) 
+					{
+                        createLine().setEndX(circle.getCenterX());
+                        createLine().setEndX(circle.getCenterX() - 5);
+                    }
+                    break;
+            }
+        });
+
+        primaryStage.setTitle("Draw");
+        primaryStage.setScene(new Scene(pane, 600, 500));
+        primaryStage.show();
+
+        circle.requestFocus();
+    }
+
+    public static void main(String args[])
+	{
+        Application.launch();
+    }
+
+    private Line createLine()
+	{
+        Line line = new Line(circle.getCenterX(), circle.getCenterY(),
+                circle.getCenterX(), circle.getCenterY());
+        line.setStrokeWidth(3);
+        line.setStroke(currentColor);
+        pane.getChildren().add(line);
+
+        return line;
+    }
+}
