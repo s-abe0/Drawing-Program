a2deb581c2566b559456371f2cb30db4499afbc1
s-abe0 <shane.abe0@gmail.com>
2017-08-01 15:41:37 -0400

    added 'clear screen' functionality

--- Draw.java
+++ Draw.java
@@ -1,5 +1,7 @@
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
@@ -13,48 +15,56 @@ import javafx.scene.paint.Color;
 public class Draw extends Application 
 {
     Color currentColor = Color.BLACK;
-    Pane pane = new Pane();
+    static Pane pane = new Pane();
     Circle circle = new Circle(50, 50, 5, Color.BLACK);
     
     @Override
     public void start(Stage primaryStage) 
-	{
+    {
         Menu colorsMenu = new Menu("Colors");
+        Menu editMenu = new Menu("Edit");
         MenuItem black = new MenuItem("Black");
         MenuItem red = new MenuItem("Red");
         MenuItem blue = new MenuItem("Blue");
+        MenuItem clearscreen = new MenuItem("Clear Screen");
         MenuBar menu = new MenuBar();
         colorsMenu.getItems().addAll(black, red, blue);
-        menu.getMenus().add(colorsMenu);
+        editMenu.getItems().addAll(clearscreen);
+        menu.getMenus().addAll(colorsMenu, editMenu);
         
         pane.getChildren().addAll(circle, menu);
 
         black.setOnAction(e -> 
-		{
+	{
            currentColor = Color.BLACK;
            circle.setFill(currentColor);
         });
         
         blue.setOnAction(e -> 
-		{
+	{
            currentColor = Color.BLUE;
            circle.setFill(currentColor);
         });
         
         red.setOnAction(e -> 
-		{
+	{
            currentColor = Color.RED;
            circle.setFill(currentColor);
         });
         
+        clearscreen.setOnAction(e ->
+        {
+            clearScreen();
+        });
+        
         circle.setOnKeyPressed(e -> 
-		{
+	{
             switch (e.getCode())
-			{
+            {
                 case DOWN:
                     circle.setCenterY(circle.getCenterY() + 5);
                     if (e.isShiftDown()) 
-					{
+                    {
                         createLine().setEndY(circle.getCenterY());
                         createLine().setEndY(circle.getCenterY() - 5);
                     }
@@ -62,7 +72,7 @@ public class Draw extends Application
                 case UP:
                     circle.setCenterY(circle.getCenterY() - 5);
                     if (e.isShiftDown()) 
-					{
+                    {
                         createLine().setEndY(circle.getCenterY());
                         createLine().setEndY(circle.getCenterY() + 5);
                     }
@@ -70,7 +80,7 @@ public class Draw extends Application
                 case LEFT:
                     circle.setCenterX(circle.getCenterX() - 5);
                     if (e.isShiftDown()) 
-					{
+                    {
                         createLine().setEndX(circle.getCenterX());
                         createLine().setEndX(circle.getCenterX() + 5);
                     }
@@ -78,7 +88,7 @@ public class Draw extends Application
                 case RIGHT:
                     circle.setCenterX(circle.getCenterX() + 5);
                     if (e.isShiftDown()) 
-					{
+                    {
                         createLine().setEndX(circle.getCenterX());
                         createLine().setEndX(circle.getCenterX() - 5);
                     }
@@ -94,12 +104,12 @@ public class Draw extends Application
     }
 
     public static void main(String args[])
-	{
+    {
         Application.launch();
     }
 
     private Line createLine()
-	{
+    {
         Line line = new Line(circle.getCenterX(), circle.getCenterY(),
                 circle.getCenterX(), circle.getCenterY());
         line.setStrokeWidth(3);
@@ -107,5 +117,11 @@ public class Draw extends Application
         pane.getChildren().add(line);
 
         return line;
+    }
+    
+    private static void clearScreen()
+    {
+        
+        pane.getChildren().removeIf(n -> n instanceof Line);
     }
 }
